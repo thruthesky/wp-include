46598be58ad3cc923c370acd4581419e767c4624
thruthesky <thruthesky@gmail.com>
2016-03-17 13:09:30 +0900

    update

--- library.php
+++ library.php
@@ -2,6 +2,8 @@
 /**
  * Removes admin bar for users only.
  */
+
+echo "HI";
 function remove_admin_bar() {
     if (!current_user_can('administrator') && !is_admin()) {
         show_admin_bar(false);

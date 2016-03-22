ca8705f9bcc2858e0f5831364922f3397550d07b
thruthesky <thruthesky@gmail.com>
2016-03-22 09:22:01 +0900

    updating wp-include

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # wp-include
+
 Wordpress Library File Folder
--- hook.php
+++ hook.php
@@ -5,10 +5,3 @@
  *
  */
 
-add_action( 'wp_footer', function () {
-    $option = get_option( '_option' );
-    $html = $option['html_bottom'];
-    if ( ! empty( $html ) ) {
-        echo $html;
-    }
-});
